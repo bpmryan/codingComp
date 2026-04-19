a802416b8861970320f4db7d1c6fccbd09627760
bpmryan <bpm_ryand2428@gmail.com>
2026-04-18 21:58:14 -0500

    Add solution for non-recursive java fib

---
+++ ja/recursion.java
@@ -0,0 +1,35 @@
+package ja;
+
+import java.util.Arrays;
+
+public class recursion {
+    public static void main(String[] args) {
+        // change fib() input to change output
+        System.out.println("Non-recursive fib: " + Arrays.toString(fib(100)));
+        
+    }
+    // non recursive fib function
+    public static int[] fib(int input) {
+        if(input <= 0) {
+            return new int[0];
+        }
+        if(input == 1) {
+            return new int[]{0};
+        }
+
+        int[] fibArray = new int[input];
+        fibArray[0] = 0;
+        fibArray[1] = 1;
+
+        for (int i = 2; i < input; i++) {
+            fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+        }
+
+        return fibArray;
+    }
+    // recursive fib function
+    // public static int[] recFib(int input){
+
+    // }
+}
+
